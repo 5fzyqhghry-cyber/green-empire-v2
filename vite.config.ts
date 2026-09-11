30cfc1e0a2165df70e8bc45dede15c0c507ba739
5fzyqhghry-cyber <5fzyqhghry@privaterelay.appleid.com>
2026-09-11 11:56:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
-  build: { outDir: 'dist' },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    target: 'es2020',
+    outDir: 'dist',
+    assetsInlineLimit: 4096,
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 });
